d005428864177bf753420cc57c72b6a76bec5301
vbv000415 <emeka.achugo@assemblyhq.com>
2023-02-20 00:39:45 +0100

    add image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
@@ -13,18 +14,23 @@ export default function Home() {
           content="7pRik2N4obUYnYqNdpRN9_E0KHftPIMZoehlM6S_BNE"
         /> */}
         <link rel="icon" href="/favicon.jpg" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <div className={styles.maindiv}>
-        
         <div className={styles.line}>
-          <Image src="/logo.png" alt="With Chito" width={800} height={800} />
+          <img src="/logo.png" alt="With Chito" />
           {/* <div className={styles.animateline}></div> */}
         </div>
       </div>
 
-      <div className="chito-name">CHITO 0.</div>
-      <footer className={styles.footer}> 
+      <div className={styles.chitoName}>CHITO O.</div>
+      <footer className={styles.footer}>
         <span className={styles.logo}>© 2022 WITHCHITO.</span>
       </footer>
     </div>
